faa7682d39d483f368bd346a2d8d45d37c1580a7
SivaReddyjella <155736932+SivaReddyjella@users.noreply.github.com>
2024-02-28 18:25:43 +0530

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -10,5 +10,7 @@ terraform {
 
 provider "aws" {
   profile = "default"
+  export AWS_ACCESS_KEY_ID='AKIAQHZUKXXJZGQO2SOH'
+  export AWS_SECRET_ACCESS_KEY='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'
   region  = "us-west-2a"
 }
